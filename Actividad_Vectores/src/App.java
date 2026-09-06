fa100ebec2dbb1e62351885ee431e1f6c406e43b
Yesid Rios <yesidrios12@gmail.com>
2026-09-06 18:54:28 -0500

    se creo el selector de menu utilizando la estructura de control switch case

--- Actividad_Vectores/src/App.java
+++ Actividad_Vectores/src/App.java
@@ -29,7 +29,62 @@ public class App {
 
             int opcion = scanner.nextInt();
 
-            
+            switch (opcion) {
+                case 1:
+                    llenarVector(scanner, vector);
+                    vectorLleno = true;
+                    break;
+                case 2:
+                    if (!vectorLleno) {
+                        System.out.println("\nPrimero debe llenar el vector.");
+                    } else {
+                        buscarValor(scanner, vector);
+                    }
+                    break;
+                case 3:
+                    if (!vectorLleno) {
+                        System.out.println("\nPrimero debe llenar el vector.");
+                    } else {
+                        mostrarMayorYMenor(vector);
+                    }
+                    break;
+                case 4:
+                    if (!vectorLleno) {
+                        System.out.println("\nPrimero debe llenar el vector.");
+                    } else {
+                        mostrarMultiplos(scanner, vector);
+                    }
+                    break;
+                case 5:
+                    if (!vectorLleno) {
+                        System.out.println("\nPrimero debe llenar el vector.");
+                    } else {
+                        System.out.println("\nLa suma total del vector es: " + sumarVector(vector));
+                    }
+                    break;
+                case 6:
+                    if (!vectorLleno) {
+                        System.out.println("\nPrimero debe llenar el vector.");
+                    } else {
+                        mostrarPromedioYSuperiores(vector);
+                    }
+                    break;
+                case 7:
+                    if (!vectorLleno) {
+                        System.out.println("\nPrimero debe llenar el vector.");
+                    } else {
+                        mostrarVector(vector);
+                    }
+                    break;
+                case 0:
+                    System.out.println("\nGracias por usar el programa.");
+                    scanner.close();
+                    return;
+                default:
+                    System.out.println("\nOpcion invalida. Intente nuevamente.");
             }
         }
     }
+
+    
+}
